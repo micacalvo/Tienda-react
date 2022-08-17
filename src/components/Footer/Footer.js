63bb549e8b2f3082679375508e95a3ecb786c94d
Micaela <micacalvo@hotmail.es>
2022-08-17 18:54:07 -0300

    Se crean componentes de Navbar y Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <a href="#">Instagram</a>
+            <a href="#">Facebook</a>
+            <a href="#">WhatsApp</a>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,18 @@
+// Creo el componente navbar que va a tener mi header 
+
+const Navbar = () => {
+    return ( 
+        <header>
+          <h3>MI BORREGUITO</h3>
+        <nav>
+         <ul>
+           <li><a href="#">Sobre mí</a></li>
+           <li><a href="#">Productos</a></li>
+           <li><a href="#">Contacto</a></li> 
+        </ul>
+        </nav>
+      </header>
+    )
+}
+
+export default Navbar;
